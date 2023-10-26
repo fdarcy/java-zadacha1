8f238c8ed48f39297fde75626b4bdeca9756676f
fdarcy <fdarcy@mail.ru>
2023-10-26 11:36:10 +0300

    zadacha

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticket = 12000;
+        int percent = 20;
+
+        int bonus = ticket / percent;
+
+        System.out.println("бонус:" + bonus);
+
+    }
+}
